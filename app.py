30e8d4ee501f9594cf64bbdc7893434091659ee7
Archit <archittyagi599@gmail.com>
2025-05-25 13:26:31 +0530

    adding more features

--- app.py
+++ app.py
@@ -8,34 +8,65 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def home_page():
-    return '<h1><center>Decon</center></h1> <a href="http://127.0.0.1:5000/encoder">Encoder</a> <a href="http://127.0.0.1:5000/decoder">Decoder</a>'
+    return render_template('html_content.html')
 
 
 
-@app.route("/encoder", methods=["GET","POST"])
-def encoder_page():
+
+
+@app.route("/tool", methods=["GET","POST"])
+def tool_operations() -> None:
     if request.method == "GET":
-        return render_template('encoder_page.html')
+        return render_template("html_content.html")
     else:
-        simple_text = request.form['taking_simple_text']
-        encoder = Encoder(simple_text)
-        encoded_text = encoder.basic_encoding(simple_text)
+        try:
+            encoded_text: str = None
+            operation_type = request.form['enc_dec_radio']
+            text = request.form['text_input']
 
-        return render_template('encoder_page.html', encoded_text=encoded_text)
+            if operation_type == "encode":
+                encoder = Encoder()
+                encoder.update_text(text)
+                encoded_text = encoder.basic_encoding()
+                updated_text = encoded_text
+
+            elif operation_type == "decode":
+                decoder = Decoder()
+                decoder.update_text(text)
+                decoded_text = decoder.basic_decoding()
+                updated_text = decoded_text
+
+        except Exception as e:
+            print(e)
+
+        return render_template("html_content.html", operation_type=operation_type, updated_text=updated_text)
+
+
+
+# @app.route("/encoder", methods=["GET","POST"])
+# def encoder_page():
+#     if request.method == "GET":
+#         return render_template('encoder_page.html')
+#     else:
+#         simple_text = request.form['taking_simple_text']
+#         encoder = Encoder(simple_text)
+#         encoded_text = encoder.basic_encoding(simple_text)
+
+#         return render_template('encoder_page.html', encoded_text=encoded_text)
 
          
 
 
-@app.route("/decoder", methods=["GET","POST"])
-def decoder_page():
-    if request.method == "GET":
-        return render_template('decoder_page.html')
-    else:
-        encoded_text = request.form['taking_encoded_text']
-        decoder = Decoder(encoded_text)
-        decoded_text = decoder.basic_decoding()
+# @app.route("/decoder", methods=["GET","POST"])
+# def decoder_page():
+#     if request.method == "GET":
+#         return render_template('decoder_page.html')
+#     else:
+#         encoded_text = request.form['taking_encoded_text']
+#         decoder = Decoder(encoded_text)
+#         decoded_text = decoder.basic_decoding()
 
-        return render_template('decoder_page.html', decoded_text=decoded_text)
+#         return render_template('decoder_page.html', decoded_text=decoded_text)
 
 
 
